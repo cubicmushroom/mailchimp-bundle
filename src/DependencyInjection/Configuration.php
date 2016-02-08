0deb3bb4abd9d40e6f3446090861fd9f9748c626
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-02-08 10:57:00 +0000

    Added config definition

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -20,6 +20,15 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('cm_mailchimp');
 
+        $rootNode
+            ->children()
+                ->arrayNode('mailchimp_api')
+                    ->children()
+                        ->scalarNode('api_key')->end()
+                    ->end()
+                ->end() // 'mailchimp_api'
+            ->end();
+
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
